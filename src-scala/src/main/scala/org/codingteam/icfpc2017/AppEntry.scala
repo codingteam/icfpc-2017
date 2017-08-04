ef20247740dd5f86a945070ad962e73f70ef4941
Akon32 <akon32@rambler.ru>
2017-08-04 21:28:10 +0300

    Fixed run with no args.

--- src-scala/src/main/scala/org/codingteam/icfpc2017/AppEntry.scala
+++ src-scala/src/main/scala/org/codingteam/icfpc2017/AppEntry.scala
@@ -1,14 +1,17 @@
 package org.codingteam.icfpc2017
 
-import org.codingteam.icfpc2017._
+object AppEntry extends App {
 
-object AppEntry extends App{
+  private def run(): Unit = {
+    args match {
+      case Array("--test-map", mapFilePath) =>
+        val map = GameMap.Map.fromJsonFile(mapFilePath)
+        print(map.toGraph())
+      case _ =>
+        println("Hello!")
+    }
 
-  private def run(mapFilePath : String): Unit ={
-    // println("Hello, world!")
-    val map = GameMap.Map.fromJsonFile(mapFilePath)
-    print(map.toGraph())
   }
 
-  run(args(0))
+  run()
 }
