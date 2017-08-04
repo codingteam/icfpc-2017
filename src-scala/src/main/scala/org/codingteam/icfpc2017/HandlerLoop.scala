3063bccd355fa6713d8da663d296bcd6417cf21a
Alexander Batischev <eual.jp@gmail.com>
2017-08-05 00:48:46 +0300

    Perform the handshake and accept the setup request

--- src-scala/src/main/scala/org/codingteam/icfpc2017/HandlerLoop.scala
+++ src-scala/src/main/scala/org/codingteam/icfpc2017/HandlerLoop.scala
@@ -2,7 +2,7 @@ package org.codingteam.icfpc2017
 
 import java.io.EOFException
 
-import org.codingteam.icfpc2017.Messages.{HelloRq, Punter}
+import org.codingteam.icfpc2017.Messages.{HelloRq, Punter, SetupRq}
 
 /**
   * Message processing cycle.
@@ -11,14 +11,30 @@ object HandlerLoop {
 
   def runLoop(server: StreamInterface, strategy: Strategy): Unit = {
     try {
-
-      // TODO: greetings
-//      val hello = HelloRq(Config.MyPunterName)
-//      server.writeToServer(hello.toJson())
-//      val helloResponse = server.readFromServer()
-      //      val HelloRs(rs) = helloResponse
-      // ...
+      val hello = HelloRq(Config.MyPunterName)
+      val helloJson = hello.toJson()
+      server.writeToServer(helloJson)
+      // ignoring the response - nothing interesting there
+      val helloResponse = server.readFromServer()
+      val setupRequest = server.readFromServer()
+      var punter: Punter = new Punter(0)
+      var map: GameMap.Map =
+        new GameMap.Map(List[GameMap.Site](),
+          List[GameMap.River](),
+          List[GameMap.SiteId]())
+      var n: BigInt = 0
+      Messages.parseServerMessageJson(setupRequest) match {
+        case Some(setup: SetupRq) => {
+          punter = new Punter(setup.punter)
+          map = setup.map
+          n = setup.punters
+          println("Our punter id is " + setup.punter + " and there's " + n + " punters in total")
+        }
+        case default => ()
+      }
       while (true) {
+        val request = server.readFromServer()
+
         // val moves = <read moves>
         // strategy.updateState(moves)
         // val myMove = strategy.nextMove()
