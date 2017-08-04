53aacfc5c362e439e92bdd68c514fae506d53515
Ilya Portnov <portnov@iportnov.ru>
2017-08-04 21:04:16 +0500

    Loading maps from file.

--- src-scala/src/main/scala/org/codingteam/icfpc2017/AppEntry.scala
+++ src-scala/src/main/scala/org/codingteam/icfpc2017/AppEntry.scala
@@ -1,10 +1,17 @@
 package org.codingteam.icfpc2017
 
+import org.codingteam.icfpc2017._
+
 object AppEntry extends App{
 
-  private def run(): Unit ={
-    println("Hello, world!")
+  private def run(mapFilePath : String): Unit ={
+    // println("Hello, world!")
+    val map = GameMap.Map.fromJsonFile(mapFilePath)
+    print(map.toJson())
+    print(map.sites)
   }
 
-  run()
+  override def main(args : Array[String]) : Unit = {
+    run(args(0))
+  }
 }
--- src-scala/src/main/scala/org/codingteam/icfpc2017/GameMap.scala
+++ src-scala/src/main/scala/org/codingteam/icfpc2017/GameMap.scala
@@ -1,5 +1,11 @@
 package org.codingteam.icfpc2017
 
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import org.json4s.jackson.Serialization
+
+import scala.io.Source
+
 object GameMap {
 
   type SiteId = Int
@@ -8,8 +14,25 @@ object GameMap {
 
   case class River(source: SiteId, target: SiteId)
 
-  class Map(sites: List[Site], rivers: List[River], mines: List[SiteId]) {
+  implicit val formats = Serialization.formats(NoTypeHints)
+    // Serialization.formats(FullTypeHints(List(classOf[Map], classOf[Site], classOf[River])))
+  // }
 
+  object Map {
+    def fromJson(str : String) : Map = {
+      return parse(str).extract[Map]
+    }
+
+    def fromJsonFile(path : String) : Map = {
+      val source = Source.fromFile(path)
+      return parse(source.reader()).extract[Map]
+    }
+  }
+
+  case class Map(sites: List[Site], rivers: List[River], mines: List[SiteId]) {
+    def toJson() : String = {
+      return Serialization.write(this)
+    }
   }
 
 }
